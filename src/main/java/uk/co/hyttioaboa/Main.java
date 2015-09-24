8bbbd2a3504af0c3db0e2ebc522a3feefbfaf020
unknown <petecopper1980@gmail.com>
2015-09-24 20:28:33 +0100

    I added a line to make sure Im not as stupid as I suspect I am to check in...

--- src/main/java/uk/co/hyttioaboa/Main.java
+++ src/main/java/uk/co/hyttioaboa/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world.");
-
+        System.out.println("I say hello too!");
         //String definition = "<node>value</node>";
         String definition = "{\"pages\":[{\"name\":\"1\"},{\"name\":\"2\"}]}";
 
