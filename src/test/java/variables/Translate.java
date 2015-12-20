7bec9619d1b244acb0798007af0daec8e9190546
James Bartlett <jimbobarto@yahoo.co.uk>
2015-12-20 01:14:51 +0000

    First attempt at variables, including JSON parsing:
    given a list of variables like so - ['variable1': 'value1', 'variable2': '{"name": "odd looking string"}']
    
    A simple variable of ${variable1} will be evaluated to "value1".
    ${variable2.name} will be evaluated to "odd looking string".
    
    This uses the JsonPath library (https://github.com/jayway/JsonPath/blob/master/README.md), which seems pretty good so far. Can't guarantee it will work for the more intricate use-cases we will have in future but will cross that bridge when we get to it.

---
+++ src/test/java/variables/Translate.java
@@ -0,0 +1,31 @@
+package variables;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import uk.co.hyttioaboa.variables.Translator;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jamesbartlett on 20/12/15.
+ */
+public class Translate {
+
+    @Test
+    public void translateSimpleVariable() {
+        Map<String, String> variables = new HashMap<String, String>();
+        variables.put("variable", "new value");
+        variables.put("variablePath", "{\"name\": \"found it\"}");
+        String testString = "${variable}";
+        String testPath = "${variablePath.name}";
+
+        Translator translator = new Translator(variables);
+        String translatedValue = translator.translate(testString);
+        System.out.println("Variable: " + translatedValue);
+
+        translatedValue = translator.translate(testPath);
+        System.out.println("Variable path: " + translatedValue);
+    }
+}
