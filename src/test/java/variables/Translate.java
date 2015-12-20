95d6ec1a8697acc212744240d48cd0ed52939e09
James Bartlett <jimbobarto@yahoo.co.uk>
2015-12-20 01:18:45 +0000

    Some actual tests in the Translate test class!

--- src/test/java/variables/Translate.java
+++ src/test/java/variables/Translate.java
@@ -8,6 +8,8 @@ import uk.co.hyttioaboa.variables.Translator;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by jamesbartlett on 20/12/15.
  */
@@ -24,8 +26,10 @@ public class Translate {
         Translator translator = new Translator(variables);
         String translatedValue = translator.translate(testString);
         System.out.println("Variable: " + translatedValue);
+        assertEquals("Simple variable should evaluate correctly", "new value", translatedValue);
 
         translatedValue = translator.translate(testPath);
         System.out.println("Variable path: " + translatedValue);
+        assertEquals("Variable path should evaluate correctly", "found it", translatedValue);
     }
 }
