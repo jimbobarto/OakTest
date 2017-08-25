f642087e178a8b7a41ef5c93b41cd1ccf52be225
James Bartlett <jimbobarto@yahoo.co.uk>
2017-08-25 08:00:48 +0100

    Some preparatory work for driver DB

--- src/main/java/uk/co/oaktest/drivers/BrowserSpecificDriver.java
+++ src/main/java/uk/co/oaktest/drivers/BrowserSpecificDriver.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import uk.co.oaktest.database.Database;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class BrowserSpecificDriver {
 
     private String browser;
@@ -42,9 +45,53 @@ public class BrowserSpecificDriver {
         }
         else {
             driver = new FirefoxDriver();
+            Package objPackage = driver.getClass().getPackage();
+            System.out.println("Implementation version: " + readVersionInfoInManifest(driver.getClass(), ""));
         }
 
+        //readVersionInfoInManifest(driver);
         return driver;
     }
 
+    public String readVersionInfoInManifest(Class driverClass, String propertiesPath){
+        // Taken from http://stackoverflow.com/a/12571330
+
+        String version = null;
+        try {
+            Properties p = new Properties();
+            //InputStream is = driverClass.getResourceAsStream("/META-INF/maven/org.seleniumhq.selenium/selenium-firefox-driver/pom.properties");
+            InputStream is = driverClass.getResourceAsStream(propertiesPath);
+            if (is != null) {
+                p.load(is);
+                version = p.getProperty("version", "");
+                if (version != null) {
+                    return version;
+                }
+            }
+        } catch (Exception e) {
+            // ignore
+        }
+
+        Package driverPackage = driverClass.getPackage();
+        //examine the package object
+
+        version = driverPackage.getSpecificationVersion();
+        if (version != null) {
+            return version;
+        }
+
+        return driverPackage.getImplementationVersion();
+    }
+
+    public String getDefaultIEVersion() {
+        return readVersionInfoInManifest(org.openqa.selenium.ie.InternetExplorerDriver.class, "");
+    }
+
+    public String getDefaultFirefoxVersion() {
+        return readVersionInfoInManifest(org.openqa.selenium.firefox.FirefoxDriver.class, "/META-INF/maven/org.seleniumhq.selenium/selenium-firefox-driver/pom.properties");
+    }
+
+    public String getDefaultChromeVersion() {
+        return readVersionInfoInManifest(org.openqa.selenium.chrome.ChromeDriver.class, "");
+    }
 }
