dbeb974fc3742686c8030afc36d2b59c6e0d574d
James Bartlett <jimbobarto@yahoo.co.uk>
2017-08-26 09:58:24 +0100

    Make selects an OptionBasedElement

--- src/main/java/uk/co/oaktest/elementInteractions/Select.java
+++ src/main/java/uk/co/oaktest/elementInteractions/Select.java
@@ -7,37 +7,8 @@ import uk.co.oaktest.containers.Container;
 import uk.co.oaktest.messages.jackson.ElementMessage;
 import uk.co.oaktest.results.ResponseNode;
 
-public class Select extends BaseElement{
-
+public class Select extends OptionBasedElement {
     public Select (ElementMessage message, ResponseNode elementResponseNode, Container elementContainer) {
         super(message, elementResponseNode, elementContainer);
     }
-
-    public boolean select() {
-
-        WebElement targetElement = findElement();
-
-        if (targetElement != null ) {
-            String selectBy = this.setUpMessage.getSelectBy();
-            String inputValue = this.setUpMessage.getValue();
-
-            startTimerInteract();
-
-            org.openqa.selenium.support.ui.Select seleniumSelect = new org.openqa.selenium.support.ui.Select(targetElement);
-            //seleniumSelect.deselectAll();
-            if (selectBy.equals("label")) {
-                seleniumSelect.selectByVisibleText(inputValue);
-            }
-            else {
-                seleniumSelect.selectByValue(inputValue);
-            }
-
-            stopTimerInteract();
-            this.responseNode.addMessage(Status.BASIC_SUCCESS.getValue(), "Selected " + inputValue + " on a dropdown identified by " + this.identifierType + " of " + this.identifier);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
 }
